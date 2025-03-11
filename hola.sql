edf7c245636413f7f3bbd6ce653778abac915a04
UnfairAdventage <158241107+UnfairAdventage@users.noreply.github.com>
2025-03-11 08:23:59 -0600

    Update hola.sql

--- hola.sql
+++ hola.sql
@@ -1,181 +1,33 @@
-CREATE DATABASE PARCIAL_2_ALEXANDER;
-USE PARCIAL_2_ALEXANDER;
+CREATE DATABASE IF NOT EXISTS telefonia;
+USE telefonia;
 
-DELIMITER $$
+CREATE TABLE usuarios (
+    idx INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    usuario VARCHAR(20) NOT NULL,
+    nombre VARCHAR(20) NOT NULL,
+    sexo VARCHAR(1),
+    nivel TINYINT,
+    email VARCHAR(50),
+    telefono VARCHAR(20),
+    marca VARCHAR(20),
+    compañia VARCHAR(50),
+    saldo FLOAT,
+    activo TINYINT(1)
+);
 
-CREATE TABLE Departamento (
-    id INT PRIMARY KEY,
-    nombre VARCHAR(50)
-)$$
-
-CREATE TABLE Profesor (
-    id INT PRIMARY KEY,
-    nit VARCHAR(9) UNIQUE,
-    nombre VARCHAR(25),
-    apellido1 VARCHAR(50),
-    apellido2 VARCHAR(50),
-    ciudad VARCHAR(50),
-    direccion VARCHAR(50),
-    telefono VARCHAR(9),
-    fecha_nacimiento DATE,
-    sexo ENUM('M', 'F'),
-    id_departamento INT,
-    FOREIGN KEY (id_departamento) REFERENCES Departamento(id)
-)$$
-
-CREATE TABLE Grado (
-    id INT PRIMARY KEY,
-    nombre VARCHAR(100)
-)$$
-
-CREATE TABLE Asignatura (
-    id INT PRIMARY KEY,
-    nombre VARCHAR(1000),
-    creditos FLOAT,
-    tipo ENUM('Obligatoria', 'Optativa'),
-    curso TINYINT(3),
-    cuatrimestre TINYINT(3),
-    id_profesor INT,
-    id_grado INT,
-    FOREIGN KEY (id_profesor) REFERENCES Profesor(id),
-    FOREIGN KEY (id_grado) REFERENCES Grado(id)
-)$$
-
-CREATE TABLE Curso_Escolar (
-    id_curso INT PRIMARY KEY,
-    anyo_inicio YEAR(4),
-    anyo_fin YEAR(4)
-)$$
-
-CREATE TABLE Alumno (
-    id INT PRIMARY KEY,
-    nit VARCHAR(9) UNIQUE,
-    nombre VARCHAR(25),
-    apellido1 VARCHAR(50),
-    apellido2 VARCHAR(50),
-    ciudad VARCHAR(50),
-    direccion VARCHAR(50),
-    telefono VARCHAR(9),
-    fecha_nacimiento DATE,
-    sexo ENUM('M', 'F')
-)$$
-
-CREATE TABLE Alumno_se_matricula_Asignatura (
-    id_alumno INT,
-    id_asignatura INT,
-    id_escolar INT,
-    FOREIGN KEY (id_alumno) REFERENCES Alumno(id),
-    FOREIGN KEY (id_asignatura) REFERENCES Asignatura(id),
-    FOREIGN KEY (id_escolar) REFERENCES Curso_Escolar(id_curso)
-)$$
-
-DELIMITER ;
-
-INSERT INTO Departamento (id, nombre) VALUES
-(10, 'Departamento 1'), (20, 'Departamento 2'), (30, 'Departamento 3'), (40, 'Departamento 4'),
-(50, 'Departamento 5'), (60, 'Departamento 6'), (70, 'Departamento 7'), (80, 'Departamento 8'),
-(90, 'Departamento 9'), (100, 'Departamento 10'), (110, 'Departamento 11'), (120, 'Departamento 12'),
-(130, 'Departamento 13'), (140, 'Departamento 14'), (150, 'Departamento 15'), (160, 'Departamento 16'),
-(170, 'Departamento 17'), (180, 'Departamento 18'), (190, 'Departamento 19'), (200, 'Departamento 20');
-
-DELIMITER $$
-CREATE PROCEDURE InsertProfesores()
-BEGIN
-    DECLARE i INT DEFAULT 100;
-    WHILE i <= 10000 DO
-        INSERT INTO Profesor (id, nit, nombre, apellido1, apellido2, ciudad, direccion, telefono, fecha_nacimiento, sexo, id_departamento)
-        VALUES (i, CONCAT('NIT', i), CONCAT('Nombre', i), CONCAT('Apellido1-', i), CONCAT('Apellido2-', i), CONCAT('Ciudad-', i),
-                CONCAT('Direccion-', i), CONCAT('Tel-', i), '1975-01-01', IF(i % 2 = 0, 'F', 'M'), (i % 200) + 10);
-        SET i = i + 100;
-    END WHILE;
-END$$
-DELIMITER ;
-CALL InsertProfesores();
-
-INSERT INTO Grado (id, nombre) VALUES
-(30, 'Grado 1'), (60, 'Grado 2'), (90, 'Grado 3'), (120, 'Grado 4'), (150, 'Grado 5'),
-(180, 'Grado 6'), (210, 'Grado 7'), (240, 'Grado 8'), (270, 'Grado 9'), (300, 'Grado 10'),
-(330, 'Grado 11'), (360, 'Grado 12'), (390, 'Grado 13'), (420, 'Grado 14');
-
-INSERT INTO Curso_Escolar (id_curso, anyo_inicio, anyo_fin) VALUES
-(50, 2001, 2002), (100, 2002, 2003), (150, 2003, 2004), (200, 2004, 2005),
-(250, 2005, 2006), (300, 2006, 2007), (350, 2007, 2008), (400, 2008, 2009),
-(450, 2009, 2010), (500, 2010, 2011);
-
-DELIMITER $$
-CREATE PROCEDURE InsertAlumnos()
-BEGIN
-    DECLARE i INT DEFAULT 1000;
-    WHILE i < 1150 DO
-        INSERT INTO Alumno (id, nit, nombre, apellido1, apellido2, ciudad, direccion, telefono, fecha_nacimiento, sexo)
-        VALUES (i, CONCAT('NIT', i), CONCAT('Alumno', i), CONCAT('Apellido1-', i), CONCAT('Apellido2-', i), CONCAT('Ciudad-', i),
-                CONCAT('Direccion-', i), CONCAT('Tel-', i), '2005-01-01', IF(i % 2 = 0, 'F', 'M'));
-        SET i = i + 1;
-    END WHILE;
-END$$
-DELIMITER ;
-CALL InsertAlumnos();
-
-DELIMITER $$
-CREATE PROCEDURE InsertAsignaturas()
-BEGIN
-    DECLARE i INT DEFAULT 1;
-    WHILE i <= 40 DO
-        INSERT INTO Asignatura (id, nombre, creditos, tipo, curso, cuatrimestre, id_profesor, id_grado)
-        VALUES (i, CONCAT('Asignatura ', i), 
-               ROUND(RAND() * 3 + 3, 1), 
-               IF(i % 2 = 0, 'Obligatoria', 'Optativa'),
-               (i % 4) + 1,
-               (i % 3) + 1,
-               100 + ((i-1) * 100),
-               30 + ((i % 14) * 30));
-        SET i = i + 1;
-    END WHILE;
-END$$
-DELIMITER ;
-CALL InsertAsignaturas();
-
-DELETE FROM Curso_Escolar;
-DELIMITER $$
-CREATE PROCEDURE InsertCursoEscolar()
-BEGIN
-    DECLARE i INT DEFAULT 50;
-    WHILE i <= 2500 DO
-        INSERT INTO Curso_Escolar (id_curso, anyo_inicio, anyo_fin)
-        VALUES (i, 2000 + (i/50), 2001 + (i/50));
-        SET i = i + 50;
-    END WHILE;
-END$$
-DELIMITER ;
-CALL InsertCursoEscolar();
-
-DELIMITER $$
-CREATE PROCEDURE InsertMatriculas()
-BEGIN
-    DECLARE i INT DEFAULT 1;
-    WHILE i <= 60 DO
-        INSERT INTO Alumno_se_matricula_Asignatura (id_alumno, id_asignatura, id_escolar)
-        VALUES (1000 + (i % 150), 
-               (i % 40) + 1,
-               50 + ((i % 50) * 50));
-        SET i = i + 1;
-    END WHILE;
-END$$
-DELIMITER ;
-CALL InsertMatriculas();
-
-DESC Departamento;
-DESC Profesor;
-DESC Asignatura;
-DESC Grado;
-DESC Alumno_se_matricula_Asignatura;
-DESC Curso_Escolar;
-DESC Alumno;
-SELECT * FROM Departamento;
-SELECT * FROM Profesor;
-SELECT * FROM Asignatura;
-SELECT * FROM Grado;
-SELECT * FROM Alumno_se_matricula_Asignatura;
-SELECT * FROM Curso_Escolar;
-SELECT * FROM Alumno;
-SHOW TABLES;
+INSERT INTO usuarios (usuario, nombre, sexo, nivel, email, telefono, marca, compañia, saldo, activo) VALUES
+('BRE2271', 'BRENDA', 'M', 2, 'brenda@live.com', '655-330-5736', 'SAMSUNG', 'IUSACELL', 100, 1),
+('OSC4677', 'OSCAR', 'H', 3, 'oscar@gmail.com', '655-143-4181', 'LG', 'TELCEL', 0, 1),
+('JOS7086', 'JOSE', 'H', 3, 'francisco@gmail.com', '655-143-3922', 'NOKIA', 'MOVISTAR', 150, 1),
+('LUI6115', 'LUIS', 'H', 0, 'enrique@outlook.com', '655-137-1279', 'SAMSUNG', 'TELCEL', 50, 1),
+('LUI7072', 'LUIS', 'H', 1, 'luis@hotmail.com', '655-100-8260', 'NOKIA', 'IUSACELL', 50, 0),
+('DAN2832', 'DANIEL', 'H', 0, 'daniel@outlook.com', '655-145-2586', 'SONY', 'UNEFON', 100, 1),
+('JAQ5351', 'JAQUELINE', 'M', 0, 'jaqueline@outlook.com', '655-330-5514', 'BLACKBERRY', 'AXEL', 0, 1),
+('ROM6520', 'ROMAN', 'H', 2, 'roman@gmail.com', '655-330-3263', 'LG', 'IUSACELL', 50, 1),
+('BLA9739', 'BLAS', 'H', 0, 'blas@hotmail.com', '655-330-3871', 'LG', 'UNEFON', 100, 1),
+('JES4752', 'JESSICA', 'M', 1, 'jessica@hotmail.com', '655-143-6861', 'SAMSUNG', 'TELCEL', 500, 1),
+('DIA6570', 'DIANA', 'M', 1, 'diana@live.com', '655-143-3952', 'SONY', 'UNEFON', 100, 0),
+('RIC8283', 'RICARDO', 'H', 2, 'ricardo@hotmail.com', '655-145-6049', 'MOTOROLA', 'IUSACELL', 150, 1),
+('VAL6882', 'VALENTINA', 'M', 0, 'valentina@live.com', '655-137-4253', 'BLACKBERRY', 'AT&T', 50, 0),
+('BRE8106', 'BRENDA', 'M', 3, 'brenda2@gmail.com', '655-100-1351', 'MOTOROLA', 'NEXTEL', 150, 1),
+('LUC4982', 'LUCIA', 'M', 3, 'lucia@gmail.com', '655-145-4992', 'BLACKBERRY', 'IUSACELL', 0, 1);
